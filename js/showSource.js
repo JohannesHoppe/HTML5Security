308a8fecff195f0181e4f9ac6dd9aeadc715228d
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2013-04-25 08:39:10 +0200

    working on first XSS example

---
+++ js/showSource.js
@@ -0,0 +1,12 @@
+﻿define(['jquery'], function() {
+
+    var showSource = function(selector) {
+        alert($(selector).html());
+    };
+
+
+    return { showSource: showSource };
+
+});
+
+window.r = window.require;
