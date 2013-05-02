35c4d39a52beae4b722ef0684f725f29bb6c58bc
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2013-05-02 22:30:19 +0200

    jsonp - now with coding horror :-)

--- examples/jsonp/loadTweets.js
+++ examples/jsonp/loadTweets.js
@@ -1,9 +1,9 @@
-﻿define(['jquery', 'knockout', 'knockout.mapping'], function($, ko, mapping) {
-  $(function() {
-    var url = 'http://search.twitter.com/search.json?q=%23dnc13&callback=?';
-    $.getJSON(url).done(function (data) {
+﻿define(['jquery', 'knockout', 'knockout.mapping', 'domReady!'], function ($, ko, mapping) {
+
+  var url = 'http://search.twitter.com/search.json?q=%23dnc13&callback=?';
+  
+  $.getJSON(url).done(function (data) {
       var viewModel = mapping.fromJS(data);
       ko.applyBindings(viewModel, $('#tweets').get(0));
-    });
   });
 });
