cb3dbc51d3564f62ceceb42776c9c4248bfa2512
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2013-05-02 13:27:34 +0200

    jsonp example

---
+++ examples/jsonp/loadTweets.js
@@ -0,0 +1,11 @@
+﻿define(['jquery', 'knockout', 'knockout.mapping'], function($, ko, mapping) {
+
+    $(function() {
+
+        $.getJSON('http://search.twitter.com/search.json?q=%23dnc13&callback=?&callback=?').done(function(data) {
+            var viewModel = mapping.fromJS(data);
+            ko.applyBindings(viewModel, $('#tweets').get(0));
+        });
+    });
+
+});
--- js/require.config.js
+++ js/require.config.js
@@ -1,10 +1,12 @@
 ﻿requirejs.config({
     //enforceDefine: true,
-    baseUrl: "js",
+    baseUrl: "/js",
     paths: {
-        'jquery': 'jquery-1.9.1'
+        'jquery': 'jquery-1.9.1',
+        'knockout': 'knockout-2.2.1',
+        'knockout.mapping': 'knockout.mapping-latest',        
     },
     shim: {
-
+        'knockout': { deps: ['jquery'] }
     }
 });
